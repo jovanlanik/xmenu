75bbed398b984915b14bb35c29da55e4cdaa237d
Jovan Lanik <jox969@gmail.com>
2021-02-02 23:45:45 +0100

    update config.mk

--- config.mk
+++ config.mk
@@ -2,30 +2,18 @@
 PROG = xmenu
 
 # paths
-PREFIX    ?= /usr/local
+PREFIX    ?= /usr
 MANPREFIX ?= ${PREFIX}/share/man
-
-LOCALINC ?= /usr/local/include
-LOCALLIB ?= /usr/local/lib
 
 # SHELL variable (mainly for non-GNU make)
 SHELL ?= /bin/sh
 
-X11INC ?= /usr/X11R6/include
-X11LIB ?= /usr/X11R6/lib
-
-FREETYPEINC ?= /usr/include/freetype2
-# OpenBSD (uncomment)
-#FREETYPEINC = ${X11INC}/freetype2
-
 # includes and libs
-INCS := -I${LOCALINC} -I${X11INC} -I${FREETYPEINC}
-LIBS := -L${LOCALLIB} -L${X11LIB} -lfontconfig -lXft -lX11 -lXinerama -lImlib2
+LIBS := -lfontconfig -lXft -lX11 -lXinerama -lImlib2
 
 # flags
-CPPFLAGS :=
-CFLAGS   := -Wall -Wextra ${INCS} ${CPPFLAGS}
-LDFLAGS  := ${LIBS}
+CFLAGS   += -I /usr/include/freetype2 -Wall -Wextra
+LDFLAGS  += ${LIBS}
 
 # compiler and linker
-CC = cc
+CC ?= cc
